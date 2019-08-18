2c4d9624c0087866683eb0c01d4c1b66bdf4e7ce
Ni-c0de-mus <44680555+Ni-c0de-mus@users.noreply.github.com>
2019-08-18 22:53:14 +0100

    4th Commint

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,21 @@
-window.addEventListener('keydown', function (e) {
+function playSound (e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  console.log(audio);
-  if (!audio) return; //stop the function //
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  console.log(key);
+  if (!audio) return; /*stop the function */
+  audio.currentTime = 0; /*rewind to the start */
+
   audio.play();
-  key.ClassList
-})
+  key.classList.add('playing');
+}
+
+function removeTransition(e) {
+  if(e.propertyName !== 'transform') return; /* Skip it if it's not a transform */
+  this.classList.remove('playing')
+  console.log(e.propertyName);
+}
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
+
+
